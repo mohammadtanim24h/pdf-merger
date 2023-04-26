e21358e69e596d6ace5edfcca9c1c01def571ec6
mohammadtanim24h <mohammad.tanim24h@gmail.com>
2023-04-26 12:35:25 +0600

    added for loop which adds all the pdf file names in a list

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("hello world")
+import PyPDF2
+import os
+
+merger = PyPDF2.PdfMerger()
+
+pdfFiles = []
+
+# Adding the names of all pdf files (except the merged.pdf) located in current folder in pdfFiles list
+for filename in os.listdir('.'):
+    if filename.endswith('.pdf') and filename != 'merged.pdf':
+        pdfFiles.append(filename)
+
+pdfFiles.sort(key=str.lower)
+
+print(pdfFiles)
