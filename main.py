8372c63f0a5faa94290770145be335d017905ca4
mohammadtanim24h <mohammad.tanim24h@gmail.com>
2023-04-26 12:53:32 +0600

    added loop to merge the pdfs

--- main.py
+++ main.py
@@ -12,4 +12,10 @@ for filename in os.listdir('.'):
 
 pdfFiles.sort(key=str.lower)
 
-print(pdfFiles)
+for filename in pdfFiles:
+    pdfFile = open(filename, 'rb')
+    pdfReader = PyPDF2.PdfReader(pdfFile)
+    merger.append(pdfReader)
+pdfFile.close()
+
+merger.write('merged.pdf')
